9860dbdc80980b6920f7c10ef18434688d0c810d
Miguel-Marcelo-Oliveira <miguel.marceloramos@gmail.com>
2026-03-16 11:40:41 -0300

    aula 2 variáveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,32 @@
+print('Hello World')
+
+print(7 + 4)
+
+print('7 + 4')
+
+print('7' + '4') # ESTOU CONCATENANDO STRIGS
+
+# São comentários de 1 linha
+
+'''
+    Isso são 
+    cometários
+    de múltiplas
+    linhas
+'''
+
+# VARIÁVEIS
+# a variável nome recebe o nome Miguel e guarda na memória
+nome = "Miguel"
+idade = 17
+peso = 75
+print(nome, idade, peso)
+print(f'Oi, {nome}')
+
+# INPUT - FORMULÁRIO SIMLES NO PROMT DE COMANDO (CMD)
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+print(nome, idade, peso)
