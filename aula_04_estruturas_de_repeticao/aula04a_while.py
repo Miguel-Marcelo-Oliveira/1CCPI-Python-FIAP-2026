cdd32f5b4c3ca6e1999b9a3e55e63545eed03684
Miguel-Marcelo-Oliveira <miguel.marceloramos@gmail.com>
2026-04-27 11:32:04 -0300

    Adicionado o conteúdo da aula 04

---
+++ aula_04_estruturas_de_repeticao/aula04a_while.py
@@ -0,0 +1,31 @@
+# cp = 0
+# while cp < 10:
+#     cp += 1
+#     if cp == 3 or cp == 5:
+#         continue
+#     print(f'Produto {cp}')
+#
+#     if cp == 7:
+#         break
+#
+# print()
+# # While decrescente de 4 até 1
+# i = 4
+# while i > 0:
+#     print(i)
+#     i -= 1
+
+def validar_nota(nota):
+    while nota < 0 or nota > 10:
+        print("A nota deve estar entre 0 e 10")
+        nota = float(input("Digite novamente nota: "))
+    return nota
+
+notaA = float(input("Digite a 1ª nota: "))
+notaA = validar_nota(notaA)
+
+notaB = float(input("Digite a 2º nota: "))
+notaB = validar_nota(notaB)
+
+media = (notaA + notaB) / 2
+print(media)
---
+++ aula_04_estruturas_de_repeticao/exercicios.py
@@ -0,0 +1,17 @@
+# 1)
+# resposta = input("Você deseja exibir a mensagem? [s] [n]: ")
+# while True:
+#     if resposta == 's':
+#         print('Olá, Mundo')
+#         resposta = input("Você deseja exibir a mensagem? [s] [n]: ")
+#     else:
+#         break
+
+# 2)
+# for i in range(0, 110, 10):
+#     print(f'i:{i}')
+
+# 3)
+numero = int(input("Digite un número: "))
+for numero in range(o, 26):
+    ...
