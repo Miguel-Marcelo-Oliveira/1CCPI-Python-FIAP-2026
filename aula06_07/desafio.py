3550e4cc58a372703e3bbf627e0a1e668193888e
Miguel-Marcelo-Oliveira <miguel.marceloramos@gmail.com>
2026-08-10 15:56:17 -0300

    Adicionada a resolução do desafio

--- aula06_07/desafio.py
+++ aula06_07/desafio.py
@@ -7,12 +7,27 @@ for email in lista_emails:
     nome_usuario, email = email.split("@")
     dominios.append(email)
     usuarios.append(nome_usuario)
+
+usuarios_tupla = tuple(usuarios)
+dominios_tupla = tuple(dominios)
+
+def analisador(lista_de_emails):
     d = dict()
     for dom in dominios:
         if dom not in d:
-            d[dom] = len(dom)
+            d[dom] = 1
+        else:
+            d[dom] += 1
 
-usuarios = tuple(usuarios)
-print(f'dommínio: {len(dominios)}')
-print(f'usuários: {usuarios}')
-print(d)
+    return d
+
+def relatorio_final():
+    print('-' * 30)
+    print('RELATÓRIO COMPLETO')
+    print('Quantidade de emails por domínio:')
+    print(analisador(lista_emails))
+    print(f'\nLista de usuários original: \n{usuarios}')
+    print(f'\nTupla de usuários inverrtida: \n{usuarios_tupla[::-1]}')
+    print('-' * 30)
+
+relatorio_final()
